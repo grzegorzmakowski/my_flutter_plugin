81ffa92d682263464ee0b0d2b3eb6d42751fee25
GitHub Action <action@github.com>
2021-09-08 11:45:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/grzegorzmakowski/myflutterplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.grzegorzmakowski.myflutterplugin.services
+
+import com.github.grzegorzmakowski.myflutterplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/grzegorzmakowski/myflutterplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/grzegorzmakowski/myflutterplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.grzegorzmakowski.myflutterplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.grzegorzmakowski.myflutterplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
